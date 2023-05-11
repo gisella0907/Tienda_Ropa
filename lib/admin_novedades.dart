71d4e110a28a184fc7e6967b903002fdfcab7802
danielquintero-o <daniel_mfc@hotmail.es>
2023-05-10 20:08:33 -0500

    update exitoso

--- lib/admin_novedades.dart
+++ lib/admin_novedades.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:tienda_ropa/inicio.dart';
 import 'package:flutter/material.dart';
 import 'package:tienda_ropa/models/novedades_response.dart';
+import 'package:tienda_ropa/objects/peticionesPut.dart';
+import 'package:tienda_ropa/objects/peticionesGet.dart';
 
 class AdministrarNovedades extends StatefulWidget {
   AdministrarNovedades(this.novedades, {super.key});
@@ -13,7 +16,6 @@ class AdministrarNovedades extends StatefulWidget {
 class _AdministrarNovedadesState extends State<AdministrarNovedades> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 141, 26, 74),
@@ -22,52 +24,55 @@ class _AdministrarNovedadesState extends State<AdministrarNovedades> {
         body: Container(
           padding: const EdgeInsets.all(20),
           child: Column(
-            children: widget.novedades
-                .map((novedad) {
-                  return Container(
-                      height: 50,
-                      padding: const EdgeInsets.only(left: 20, right: 20),
-                      margin: const EdgeInsets.only(bottom: 10),
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: Colors.orange),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            IconButton(
-                                onPressed: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            EditNovedad(novedad))),
-                                icon: Icon(Icons.edit)),
-                            Text(novedad.descripcion)
-                          ],
-                        ),
-                  );
-                    })
-                .toList(),
+            children: widget.novedades.map((novedad) {
+              return Container(
+                height: 50,
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                margin: const EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.orange),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EditNovedades(novedad))),
+                        icon: Icon(Icons.edit)),
+                    Text(novedad.descripcion)
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ));
   }
 }
 
-class EditNovedad extends StatelessWidget {
-  Novedades _novedades;
-  EditNovedad(this._novedades);
-
-  final _formKey = GlobalKey<FormState>();
-          
-  final TextEditingController estadoController = TextEditingController();
+class EditNovedades extends StatefulWidget {
+  Novedades novedades;
   final TextEditingController descripcionController = TextEditingController();
-      
+  final TextEditingController imagenController = TextEditingController();
+  EditNovedades(this.novedades) {
+    descripcionController.text = novedades.descripcion;
+    imagenController.text = novedades.imagen;
+  }
+
+  @override
+  State<EditNovedades> createState() => _EditNovedad();
+}
+
+class _EditNovedad extends State<EditNovedades> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-       
       appBar: AppBar(
-        backgroundColor: Colors.orange[300],
+        backgroundColor: Color.fromARGB(255, 141, 26, 74),
       ),
       body: Container(
         padding: const EdgeInsets.all(20),
@@ -75,7 +80,7 @@ class EditNovedad extends StatelessWidget {
           Container(
             margin: const EdgeInsets.only(top: 25, bottom: 20),
           ),
-          const Text("Detalle Tareas",
+          const Text("Modificar Novedad",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
           Container(
             child: Padding(
@@ -85,10 +90,19 @@ class EditNovedad extends StatelessWidget {
                 child: Column(
                   children: [
                     TextFormField(
-                      controller: descripcionController,
+                      controller: widget.descripcionController,
                     ),
-                    TextFormField(                      
-                      controller: estadoController,
+                    TextFormField(
+                      controller: widget.imagenController,
+                    ),
+                    CheckboxListTile(
+                      title: Text('Estado'),
+                      value: widget.novedades.estado,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          widget.novedades.estado = value!;
+                        });
+                      },
                     ),
                     SizedBox(height: 16),
                   ],
@@ -96,9 +110,66 @@ class EditNovedad extends StatelessWidget {
               ),
             ),
           ),
+          Container(
+              height: 50,
+              width: screenSize.width - 40,
+              margin: const EdgeInsets.only(
+                  top: 45, left: 20, right: 20, bottom: 25),
+              child: FilledButton(
+                  style: FilledButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 141, 26, 74),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onPressed: () async {
+                    var novedadesPut = await updateNovedades(
+                        widget.novedades.id,
+                        widget.descripcionController.text,
+                        widget.imagenController.text,
+                        widget.novedades.estado);
+                    if (novedadesPut == "Actualizacion Exitosa") {
+                      showDialog(
+                        context: context, 
+                        builder: (BuildContext context) => AlertDialog(
+                          title: Text('Actualizacion Exitosa'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () async {
+                                  List<Novedades> novedades =
+                                      await getNovedadesData();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            AdministrarNovedades(novedades)),
+                                  );
+                                },
+                              child: const Text('Aceptar'),
+                            ),
+                          ]
+                        )
+                      );
+                      /*Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Inicio()),
+                      );*/
+                    }else{
+                      showDialog(
+                        context: context, 
+                        builder: (BuildContext context) => AlertDialog(
+                          title: Text('No se pudo realizar la actualizacion'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, 'OK'),
+                              child: const Text('Aceptar'),
+                            ),
+                          ]
+                        )
+                      );
+                    }
+                  },
+                  child: const Text("Actualizar"))),
         ]),
       ),
     );
   }
 }
-
